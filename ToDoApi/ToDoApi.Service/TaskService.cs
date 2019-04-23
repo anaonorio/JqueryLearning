32eb5346d65defe39d41d00e2895eb2be4bd3f7b
Ana Onório <ana.onorio@bellinatiperez.com.br>
2019-04-23 15:21:35 -0300

    api

---
+++ ToDoApi/ToDoApi.Service/TaskService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Service
+{
+    public class TaskService
+    {
+        private static List<Model.Task> Tasks = new List<Model.Task>();
+
+        public static void Add(Model.Task task)
+        {
+            task.Id = CreateNewId();
+            Tasks.Add(task);
+        }
+
+        private static int CreateNewId()
+        {
+            int newId = 0;
+            do
+            {
+                newId = new Random().Next();
+            } while (Get(newId) != null);
+
+            return newId;
+        }
+
+        public static List<Model.Task> GetAll()
+        {
+            return Tasks;
+        }
+
+        public static Model.Task Get(int id)
+        {
+            return Tasks.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+
+        public static bool Delete(int id)
+        {
+            Model.Task taskDelete = Get(id);
+            if (taskDelete != null)
+            {
+                Tasks.Remove(taskDelete);
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
